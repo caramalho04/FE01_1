ba991e5f10dcb2fdff587cdb61cf7bf5e83aba10
Francisco  Caramalho <franciscocaramalho2018@gmail.com>
2022-11-15 09:36:48 +0000

    exercicios resolvidos

--- 2A.c
+++ 2A.c
@@ -1,46 +1,12 @@
-//operações aritmeticas
+// subtracao adicao multiplicacao divisao
 
 #include <stdio.h>
-#include <locale.h>
-
-int adicao(float x, float y){
-    float soma;
-    soma= x+y;
-    return soma;
-}
-
-int subtracao(float x, float y){
-    float sub;
-    sub=x-y;
-    return sub;
-}
-
-int multiplicacao(float x, float y){
-    float mult;
-    mult=x*y;
-    return mult;
-}
-
-int divisao(float x, float y){
-    float div;
-    div=x/y;
-    return div;
-}
 
 int main(){
-    setlocale(LC_ALL, "Portuguese");
-    float x,y,a,s,m,d,soma,sub,mult,div;
+    float x,y;
+
     printf("Introduza dois valores:\n");
     scanf("%f %f", &x, &y);
 
-    a = adicao(soma);
-
-    s = subtracao(sub);
-
-    m = multiplicacao(mult);
-
-    d = divisao(div);
-
-    printf("Adição= %.2f\n Subtração= %.2f \n Multiplicação=%.2f\n Divisão=%.2f ", soma,sub,mult,div);
-
+    printf("Adicao= %.2f \nSubtracao= %.2f \nMultiplicacao=%.2f\nDivisao=%.2f ", x+y,x-y,x*y,x/y);
 }
